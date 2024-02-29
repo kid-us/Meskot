d694d75046266d3526fb22645dac85957b8b4c6d
kid-us <kidushh29@gmail.com>
2024-02-29 23:06:28 +0300

    Footer

--- src/components/Footer/SocialMedia.jsx
+++ src/components/Footer/SocialMedia.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 const SocialMedia = () => {
   return (
-    <div className="col-12 ps-lg-5 mt-3">
+    <div className="col-12 text-center ps-lg-5 mt-3">
       <Link className={`social-media text-danger bi-instagram p-1 fs-3`}></Link>
       <Link
         className={`social-media text-warning bi-snapchat mx-5 p-1 fs-3`}

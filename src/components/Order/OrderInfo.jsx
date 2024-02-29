9b73995b8b815cebac77528a0c104c2c3aba9c20
kid-us <kidushh29@gmail.com>
2024-02-29 17:56:15 +0300

    Navbar Fixed

--- src/components/Order/OrderInfo.jsx
+++ src/components/Order/OrderInfo.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import NavBar from "../NAvBar";
+import NavBar from "../NavBar";
 import Footer from "../Footer";
 import OrderBy from "./OrderBy";
 import ViewOrder from "./ViewOrder";
